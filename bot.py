cd66f544dc970bba250993cc27c4c7ef9313d463
Sayyed Faizan <103510430+SahilFaiz@users.noreply.github.com>
2024-03-04 22:55:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,7 +43,7 @@ async def broadcast_command(client, message):
         print("u r not owner")
 
 async def send_broadcast_message(client, text):
-    if text is not None and text.strip():
+    if text.strip():
         # Retrieve user IDs from the database
         cursor = mydb.cursor()
         cursor.execute("SELECT user_id FROM users")
